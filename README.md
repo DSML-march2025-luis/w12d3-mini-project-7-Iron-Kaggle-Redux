f157db4e704b21bc5a1fe29e94811e759550f828
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2025-06-03 18:10:52 +0200

    docs: improve api documentation

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
 
-
-# Project: Iron Kaggle Redux (Flask API to allow forecasting stores sales)
-
+# Project: Iron Kaggle Redux - Flask API to allow store sales prediction using Machine Learning
 
 
 ## About
 
 This project is part of my Master's in Data Science & Machine Learning at Ironhack.
 
+<br>
+
+
 ## Project Brief
 
 Goal:
 - Create a public API to forecast the sales of a store chain based on historical data.
 
+<br>
 
 Requirements:
 - The API should be based on the model we created in our previous project, [Iron-Kaggle](https://github.com/DSML-march2025-luis/w10d3-mini-project-6-Iron-Kaggle)
@@ -20,6 +22,7 @@ Requirements:
 - It should handle well incorrect requests (e.g. wrong http method, requests with missing data, invalid format, etc)
 - It must be deployed to allow public access (e.g. Docker, AWS)
 
+<br>
 
 Example request body:
 
@@ -31,9 +34,12 @@ Example request body:
     "nb_customers_on_day": 1254,
     "open": 1,
     "promotion": 0,
-    "state_holiday": "0",
-    "school holiday": 1
+    "state_holiday": "a",
+    "school_holiday": 1
 }
 ```
+
+<br>
+
 
 
--- app.py
+++ app.py
@@ -11,51 +11,13 @@ app = Flask(__name__)
 # GET /
 @app.route("/", methods=["GET"])
 def home():
-    return jsonify("API to predicts sales for a store. Check /docs for more info.")
+    return render_template('index.html')
 
 
 # GET /docs
 @app.route("/docs", methods=["GET"])
 def docs():
-    return jsonify({
-        "endpoints": {
-            "/": {
-                "method": "GET",
-                "description": "Homepage"
-            },
-            "/docs": {
-                "method": "GET",
-                "description": "Returns available endpoints and usage."
-            },
-            "/model/predict-sales": {
-                "method": "POST",
-                "description": "Predicts sales for a store using an ML model.",
-                "request_body": {
-                    "required": True,
-                    "fields": {
-                        "store_ID": "integer - unique identifier of the store",
-                        "day_of_week": "integer - day of the week, between 1 and 7",
-                        "date": "string - date for the prediction in the format DD-MM-YYYY",
-                        "nb_customers_on_day": "integer - expected number of customers",
-                        "open": "integer - if the shop is open (1) or not (0)",
-                        "promotion": "integer - if there's a promotion (1) or not (0)",
-                        "state_holiday": "integer - if there's a state holiday",
-                        "school_holiday": "integer - if there's school holiday (1) or not (0)",
-                    },
-                    "example": {
-                        "store_ID": 49,
-                        "day_of_week": 4,
-                        "date": "26/06/2014",
-                        "nb_customers_on_day": 1254,
-                        "open": 1,
-                        "promotion": 0,
-                        "state_holiday": 0,
-                        "school holiday": 1,
-                    },
-                }
-            }
-        }
-    })
+    return render_template('docs.html')
 
 
 class FeaturesSchema(Schema):
